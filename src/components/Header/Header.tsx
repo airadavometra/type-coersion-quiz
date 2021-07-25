6d273425f80390a0fc104b8481883ae7d2c272f5
Daria Artemova <airadavometra@gmail.com>
2021-07-25 18:05:07 +0300

    made menuitem component

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,47 +1,17 @@
-import { Link } from '@reach/router';
-import React, { FunctionComponent, useState } from 'react';
+import React, { FunctionComponent } from 'react';
 import classes from './Header.module.scss';
 import Logo from '@icons/black-cat.svg';
-import classNames from 'classnames';
 import { PageRoot } from '@constants';
+import { MenuItem } from '@components/MenuItem/MenuItem';
 
 export const Header: FunctionComponent = () => {
-  const [selectedItem, setSelectedItem] = useState(window.location.pathname);
-
-  const isSelected = (path: string) => selectedItem === path;
-
-  const goToPage = (path: string) => {
-    setSelectedItem(path);
-  };
-
   return (
     <header className={classes.main}>
       <Logo className={classes.logo} />
       <ul className={classes.menu}>
-        <li
-          className={classNames(classes.link, {
-            [classes.selectedLink]: isSelected(PageRoot.Main),
-          })}
-          onClick={() => goToPage(PageRoot.Main)}
-        >
-          <Link to={PageRoot.Main}>About</Link>
-        </li>
-        <li
-          className={classNames(classes.link, {
-            [classes.selectedLink]: isSelected(PageRoot.Eval),
-          })}
-          onClick={() => goToPage(PageRoot.Eval)}
-        >
-          <Link to={PageRoot.Eval}>eval()</Link>
-        </li>
-        <li
-          className={classNames(classes.link, {
-            [classes.selectedLink]: isSelected(PageRoot.CatQuiz),
-          })}
-          onClick={() => goToPage(PageRoot.CatQuiz)}
-        >
-          <Link to={PageRoot.CatQuiz}>Cat Quiz</Link>
-        </li>
+        <MenuItem text="About" path={PageRoot.Main} />
+        <MenuItem text="eval()" path={PageRoot.Eval} />
+        <MenuItem text="Cat Quiz" path={PageRoot.CatQuiz} />
       </ul>
     </header>
   );
