e9001a56530b22ad5de0d22c1f941e81b9b4ba4d
Daria Artemova <darya.artemova@raiffeisen.ru>
2022-03-28 18:58:09 +0300

    add path track

--- src/main.tsx
+++ src/main.tsx
@@ -4,12 +4,15 @@ import { Provider } from 'react-redux';
 import { App } from '@components/App/App';
 import '@style/index.scss';
 import { store } from '@store/index';
+import { createHistory, LocationProvider } from '@reach/router';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <LocationProvider>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </LocationProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
