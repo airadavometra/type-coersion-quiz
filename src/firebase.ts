fcad9cd0e6872f56861af046554195a4c6148bea
Daria Artemova <airadavometra@gmail.com>
2024-10-22 17:50:52 +0200

    add firebase analytics

---
+++ src/firebase.ts
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCDaIGDbbXynWK1AdrWEPBsnaXi06V04W0",
+  authDomain: "typescoersionquiz.firebaseapp.com",
+  projectId: "typescoersionquiz",
+  storageBucket: "typescoersionquiz.appspot.com",
+  messagingSenderId: "21468837137",
+  appId: "1:21468837137:web:f0cf83f6d2b59071f5c8b0",
+  measurementId: "G-Y865NKVDNB",
+};
+
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
